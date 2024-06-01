723825a92fec2e163ca4f9ea34c329294670326e
jwong28 <jeffersonwong28@gmail.com>
2024-05-31 23:31:37 -0400

    Add wine

--- recipes.py
+++ recipes.py
@@ -5,12 +5,32 @@ import time
 
 keyboard = Controller()
 
+def press(key):
+    keyboard.press(key)
+    time.sleep(0.05)
+    keyboard.release(key)
+    time.sleep(0.05)
+
+def pressServeCook():
+    press(Key.space)
+
+
 def chicken():
     for x in range(6):
-        keyboard.press('q')
-        time.sleep(0.05)
-    keyboard.press(Key.space)
+        press('q')
+    press('e')
+    pressServeCook()
 
+def wine(wineName):
+    wineList = ['cheap']
+    index = wineList.index(wineName)
+    for x in range(index):
+        press('q')
+    for x in range(27):
+        press('w')
+    pressServeCook()
+ 
 if __name__ == "__main__":
+    time.sleep(3)
     chicken()
 
